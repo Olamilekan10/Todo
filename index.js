2be8d4a0bd6e4ff62b9062355d5ca12fbb88ce81
olamilekan <issaabubakry@gmail.com>
2022-07-22 13:35:57 +0100

    The user input todo value will be added to the initial todos

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 
  const generateTemplate = todo => {
       
-      // const html = `
-      // <li class="list-group-item d-flex justify-content-between align-items-center">
-      // <span>${todo}</span>
-      // <i class="fa fa-trash delete" aria-hidden="true"></i> 
-      // </li>`;
+      const html = `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="fa fa-trash delete" aria-hidden="true"></i> 
+      </li>`;
 
        list.innerHTML += html;
 
